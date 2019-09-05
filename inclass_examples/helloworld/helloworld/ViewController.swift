2718025a918c7c3c90de019ec7788da9302c18d2
joag8442 <joag8442@colorado.edu>
2019-09-05 12:10:02 -0600

    update in class

---
+++ inclass_examples/helloworld/helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Joshua Eric Aguilar Wynn on 9/5/19.
+//  Copyright © 2019 Joshua Eric Aguilar Wynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
